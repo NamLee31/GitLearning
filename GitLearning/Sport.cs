46e33490495782d405768bb420bae9d24169bf37
NamLee <namlee3102@gmail.com>
2026-04-06 22:15:45 +0700

    first commit

---
+++ GitLearning/Sport.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitLearning
+{
+    internal class Sport
+    {
+        private string _ballShape;
+        public string BallShape { get { return _ballShape; }  set { _ballShape = value; } }
+        public int Players { get; set; }
+        public Sport() 
+        {
+            BallShape = "round";
+            Players = 11;
+            Console.WriteLine(BallShape);
+        }
+    }
+
+}
